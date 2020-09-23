686f813a0eb4ffa3ac8dee948e81ab6cfa03ca15
Kamaal Farah <kamaal.f1@gmail.com>
2020-09-23 21:35:40 +0200

    Getting privacy from app content

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -1,55 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import ContactMe from "./ContactMe";
+
+import { privacy } from "../appContent";
 
 function PrivacyPolicy() {
   return (
     <>
-      <h1>Privacy Policy</h1>
-      <p>
-        This policy applies to all information collected or submitted on
-        Kamaal's apps for iPhone, iPad and Mac.
-      </p>
-      <p>
-        This page is used to inform visitors regarding my policies with the
-        collection, use, and disclosure of Personal Information if anyone
-        decided to use my Service.
-      </p>
-      <p>
-        If you choose to use my Service, then you agree to the collection and
-        use of information in relation to this policy. The Personal Information
-        that I collect is used for providing and improving the Service. I will
-        not use or share your information with anyone except as described in
-        this Privacy Policy.
-      </p>
-      <h2>Information Collection and Use</h2>
-      <p>
-        For a better experience, while using our Service, I may require you to
-        provide us with certain personally identifiable information. The
-        information that I request will be retained on your device and is not
-        collected by me in any way.
-      </p>
-      <p>
-        The app does use third party services that may collect information used
-        to identify you.
-      </p>
-      <h2>Log Data</h2>
-      <p>
-        I want to inform you that whenever you use my Service, in a case of an
-        error in the app I collect data and information (through third party
-        products) on your phone called Log Data. This Log Data may include
-        information such as your device Internet Protocol (“IP”) address, device
-        name, operating system version, the configuration of the app when
-        utilizing my Service, the time and date of your use of the Service, and
-        other statistics.
-      </p>
-      <h2>Changes to This Privacy Policy</h2>
-      <p>
-        I may update our Privacy Policy from time to time. Thus, you are advised
-        to review this page periodically for any changes. I will notify you of
-        any changes by posting the new Privacy Policy on this page. These
-        changes are effective immediately after they are posted on this page.
-      </p>
+      <h1>{privacy.title}</h1>
+      {privacy.description.map((paragraph, index) => {
+        return <p key={index}>{paragraph}</p>;
+      })}
+      {privacy.content.map(({ header, content }) => {
+        return (
+          <Fragment key={header}>
+            <h2>{header}</h2>
+            {content.map((paragraph, index) => {
+              return <p key={index}>{paragraph}</p>;
+            })}
+          </Fragment>
+        );
+      })}
       <ContactMe from={"Privacy Policy"} />
     </>
   );
--- src/components/TermsAndConditions.js
+++ src/components/TermsAndConditions.js
@@ -1,20 +1,25 @@
 import React, { Fragment } from "react";
 
 import { termsAndCondition } from "../appContent";
-import { lastUpdated } from "../config";
 import ContactMe from "./ContactMe";
+
+import { lastUpdated } from "../config";
 
 function TermsAndConditions() {
   return (
     <>
       <h1>{termsAndCondition.title}</h1>
       <h3>Last updated: {lastUpdated}</h3>
-      <p>{termsAndCondition.description}</p>
+      {termsAndCondition.description.map((description, index) => {
+        return <p key={index}>{description}</p>;
+      })}
       {termsAndCondition.content.map(({ header, content }) => {
         return (
           <Fragment key={header}>
             <h2>{header}</h2>
-            <p>{content}</p>
+            {content.map((paragraph, index) => {
+              return <p key={index}>{paragraph}</p>;
+            })}
           </Fragment>
         );
       })}
